ba5a6fea6dc462e14e734f3f4fdde687a1bab7b9
yama-24 <yamamoto.masanori.se@gmail.com>
2023-10-21 14:51:17 +0900

    1st commmit

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from PIL import Image
 # FASTAPI_URL = "http://localhost:8000/predict/" # local
 FASTAPI_URL = "https://brain-st-fast.onrender.com/predict/" # Render
 
-#　推論したラベルから脳の病名を返す関数
+# 推論したラベルから脳の病名を返す関数
 def getName(label):
     if label==0:
         return '神経膠腫（glioma）'
@@ -18,22 +18,22 @@ def getName(label):
     elif label==3:
         return '下垂体腫瘍（pituitary）'
 
+# サイドバー（入力画面）
+st.sidebar.title("脳画像から腫瘍を分類")
+uploaded_file = st.sidebar.file_uploader("脳画像をアップロードしてください", type=["jpg", "jpeg", "png"])
 
-st.title("脳画像から腫瘍の種類を分類する")
-
-uploaded_file = st.file_uploader("脳画像をアップロードしてください", type=["jpg", "jpeg", "png"])
-
+# 画面表示
 if uploaded_file:
     image = Image.open(uploaded_file)
-    st.image(image, caption="アップロードされた画像", use_column_width=True)
+    st.image(image, caption="アップロードされた画像", use_column_width="auto")
 
     # 予測の実行
     files = {"file": uploaded_file.getvalue()}
     response = requests.post(FASTAPI_URL, files=files)
 
     # デバッグ用
-    print("Status code:", response.status_code)
-    print("Response content:\n", response.text)
+    # print("Status code:", response.status_code)
+    # print("Response content:\n", response.text)
 
     predicted_class_idx = response.json()["predicted_class_idx"]
 
