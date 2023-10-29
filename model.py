8eb4b2bbc3ff4b43deeb90370fcf27ce8d4b7f1b
yama-24 <yamamoto.masanori.se@gmail.com>
2023-10-29 20:06:48 +0900

    1st commmit

--- model.py
+++ model.py
@@ -9,7 +9,8 @@ import torch.nn.functional as F
 transform = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 class Net(nn.Module):
@@ -18,37 +19,27 @@ class Net(nn.Module):
         super().__init__()
 
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(16)
 
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(32)
 
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
-        self.bn3 = nn.BatchNorm2d(64)
-
-        self.fc = nn.Linear(50176, 4)
+        self.fc1 = nn.Linear(100352, 128)
+        self.fc2 = nn.Linear(128, 4)
 
     def forward(self, x):
 
         # 1層目
         h = self.conv1(x)
         h = F.relu(h)
-        h = self.bn1(h)
         h = F.max_pool2d(h, kernel_size=2, stride=2)
 
         # 2層目
         h = self.conv2(h)
         h = F.relu(h)
-        h = self.bn2(h)
-        h = F.max_pool2d(h, kernel_size=2, stride=2)
-
-        # 3層目
-        h = self.conv3(h)
-        h = F.relu(h)
-        h = self.bn3(h)
         h = F.max_pool2d(h, kernel_size=2, stride=2)
 
         # 全結合層
-        h = h.view(-1, 50176)
-        h = self.fc(h)
+        h = h.view(-1, 100352)
+        h = self.fc1(h)
+        h = F.relu(h)
+        h = self.fc2(h)
         return h
